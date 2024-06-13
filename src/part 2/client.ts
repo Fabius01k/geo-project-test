961be2c5a37bdfaafb5767e2a2bf814a9ecad31f
Fabius829 <fabius01k@gmail.com>
2024-06-13 22:37:03 +0300

    check

--- src/part 2/client.ts
+++ src/part 2/client.ts
@@ -1,7 +1,6 @@
 import { GeoIPServiceSDK } from "./sdk";
 
 const geoIPClient = new GeoIPServiceSDK('http://localhost:3000');
-
 const clientIP = '77.51.199.15';
 geoIPClient.getLocationByIP(clientIP)
   .then((location) => {
